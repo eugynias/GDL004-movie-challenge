9a67853ef6531c5a1c0d96f035cab648e29054eb
GeorginaPQ <giorginapqe@gmail.com>
2020-05-05 09:14:17 -0500

    S[Buscador] funcionalidad para filtrar con el valor del input

--- page-movies/src/Components-JS/FetchData.js
+++ page-movies/src/Components-JS/FetchData.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 
 function App() {
+  const [searchName, setSearchName] = useState('Batman')
   useEffect(() => {
     getMovie();
   }, []);
+  
+  
 
-
-  const MOVIE_API_URL = "http://www.omdbapi.com/?apikey=745c4feb&s='harry'"
+  const MOVIE_API_URL = `http://www.omdbapi.com/?apikey=745c4feb&s=${searchName}`
   
 
   const getMovie = async () => {
@@ -19,8 +21,16 @@ function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <form className="serch-form">
-        <input className="serch-bar" type="text" />
+      <form className="serch-form"
+      onSubmit={ (e) => { 
+        e.preventDefault()
+        getMovie()
+      }}    
+      >        
+        <input className="serch-bar" type="text" 
+          onChange={((e)=>{         
+          setSearchName(e.target.value)
+        })}/>
         <button className="serch-button" type="submit">
           Serch
         </button>
