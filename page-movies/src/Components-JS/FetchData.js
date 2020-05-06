ea26e7192ec4e3ccbaff29ffc4640d78bbf15c97
unknown <eugynias>
2020-05-05 22:21:56 -0400

    Conflictos despues del pull

--- page-movies/src/Components-JS/FetchData.js
+++ page-movies/src/Components-JS/FetchData.js
@@ -1,15 +1,10 @@
 import React, { useEffect, useState } from "react";
-import CadsMovies from './CardsMovies'
 
 
-
-function Fetch() {
+function App() {
   const [searchName, setSearchName] = useState('Batman');
   const [page,setPage] = useState(1) //con el botón agregaría mas paginas
   const [listMovie, setListMovie] = useState([]);
-  const [disabledNext, setDisabledNext]= useState(false);
-  const [disabledBack, setDisabledBack]= useState(false);
-  
   useEffect(() => {
     getMovie();
   }, []);
@@ -25,66 +20,52 @@ function Fetch() {
     console.log(data);
     setListMovie(data.Search)
     //setListMovie([...listMovie, ...data.Search])//nos agrega a la misma lista
-    return data;  
+    return data;
   };
 
   const pageChange = (e) =>{
+    console.log(typeof e.target.innerHTML);
     if ( e.target.innerHTML === '+'){ 
-      if(page <10){
-        setPage(page + 1) 
-        getMovie()
-        setDisabledBack(false)
-      }else{
-        setDisabledNext(true)
-      }
-      
-    } else if (e.target.innerHTML === '-'){
-        if(page >= 1){
-          setPage(page - 1) 
-          setDisabledBack(false)
-          getMovie()
-        }else{
-          setDisabledBack(true)
-          setPage(1) 
-        }
+      // if (page > 30){
+
+      // }     
+      setPage(page + 1) 
+      getMovie()
+    } else if (e.target.innerHTML === '-') {
+      setPage(page - 1) 
+      getMovie()
     }
+    
   } 
   
- 
 
-return (
+  return (
     <div className="App">
-        <h1>Hello React</h1>
-        <form className="serch-form"
-            onSubmit={ (e) => { 
-                e.preventDefault()
-                getMovie()
-            }}    
-        >        
-            <input className="serch-bar" type="text" 
-            onChange={((e)=>{         
-            setSearchName(e.target.value)
-            })}/>
-            <button className="serch-button" type="submit">
-            Serch
-            </button>
-        </form>
+      <h1>Hello React</h1>
+      <form className="serch-form"
+      onSubmit={ (e) => { 
+        e.preventDefault()
+        getMovie()
+      }}    
+      >        
+        <input className="serch-bar" type="text" 
+          onChange={((e)=>{         
+          setSearchName(e.target.value)
+        })}/>
+        <button className="serch-button" type="submit">
 
-      <div>
-        {listMovie.map(item =>(  
-            <div   key={item.Title}>
-                <CadsMovies  data={item}/>
-            </div> 
-            
-         ))}
-      </div>
+        </button>
+      </form>
 
-
-      <button disabled={disabledBack} onClick={pageChange} className="serch-button" type="button">-</button> 
-      <button disabled={disabledNext} onClick={pageChange} className="serch-button" type="button">+</button>
+      <ul>
+        {listMovie.map(item => <li> {item.Title}</li>);
+           
+      </ul>
+      <button disabled={true} onClick={pageChange} className="serch-button" type="button">-</button> 
+      <button disableb={false} onClick={pageChange} className="serch-button" type="button">+</button>
     </div>
   );
 }
 
 
-export default Fetch
+export default App
