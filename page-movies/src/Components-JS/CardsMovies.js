14d7ec513bfe8fdbd01bd43de789cc8692f4c156
Teresa Carbajal <Tere@MacBook-Air-de-Teresa.local>
2020-05-05 23:12:12 -0500

    [CardsMovies] ordenar por columnas bootstrap css

--- page-movies/src/Components-JS/CardsMovies.js
+++ page-movies/src/Components-JS/CardsMovies.js
@@ -1,14 +1,18 @@
 import React from "react";
-import Styles from '../Components-CSS/CardMoviePoster.module.css'
+import Styles from '../Components-CSS/CardMoviePoster.module.css';
 
 function CadsMovies({data}) {
     
     return (
-      <div className={Styles.card}>
-        <img  alt='poster' src={data.Poster}/>
-        <p>{data.Title}</p>
-      </div>
-    )
+        <div className="container d-flex justify-content-center mt-5">
+            <div className="card shadow-lg mb-5 bg-white rounded"  style={{width: '18rem'}}>
+                <img src={data.Poster} style={{height: '22rem'}} alt="poster" />
+                <div className="card-body" style={{height: '5rem'}}className={Styles.cardTitle}>
+                    <p className="card-text bg-white py-3 px-4">{data.Title}</p>
+                </div>
+                </div>
+            </div>
+    );
   }
   
   export default CadsMovies;
