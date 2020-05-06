01aa421e4ebd69ca588542c67f7dd4480af2a0bd
Teresa Carbajal <Tere@MacBook-Air-de-Teresa.local>
2020-05-06 10:29:35 -0500

    [CardsMovies] conectando con otro punto de api [t]

--- page-movies/src/Components-JS/CardsMovies.js
+++ page-movies/src/Components-JS/CardsMovies.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import Styles from '../Components-CSS/CardMoviePoster.module.css';
 
-function CadsMovies({data}) {
+function CadsMovies({dataItem}) {
+    
+ //const titleMovie = dataItem.Title;
+const [titleMovie, settitleMovie] = useState(dataItem.Title)
+const [newFetch, setNewFetch] = useState('')
+
+/* 'https://www.omdbapi.com/?apikey=745c4feb&t=Friends&season=5' */
+
+const MOVIE_API_URL2 = `https://www.omdbapi.com/?apikey=745c4feb&t=${titleMovie}`
+
+  const getMovie2 = async () => {
+    const response = await fetch(MOVIE_API_URL2);
+    const data = await response.json();
+    console.log(data);
+    setNewFetch(data);
+  };
+
+  getMovie2();
+
+/*   const getData = () => {
+   return new Promise ((resolve, reject) => {
+        reject error
+        resolve getMovie2();  
+    })
+    }
+ 
+    getData().then .catch */
+/* const objPropsFe = {
+   score: newFetch.imdbRatingn,
+    age: newFetch.Rated,
+    time: runtem
+} */
     
     return (
         <div className="container d-flex justify-content-center mt-5">
             <div className="card shadow-lg mb-5 bg-white rounded"  style={{width: '18rem'}}>
-                <img src={data.Poster} style={{height: '22rem'}} alt="poster" />
-                <div className="card-body" style={{height: '5rem'}}className={Styles.cardTitle}>
-                    <p className="card-text bg-white py-3 px-4">{data.Title}</p>
+                <img src={dataItem.Poster} style={{height: '22rem'}} alt="poster" />
+                <div className="card-body" style={{height: '5rem'}} className={Styles.cardTitle}>
+                    <p className="card-text bg-white py-3 px-4">{dataItem.Title}</p>
+                    <p className="card-text bg-white py-3 px-4">{newFetch.imdbRatingn}</p>
                 </div>
                 </div>
             </div>
