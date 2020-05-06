f33bb9ea894ac11a06819831db6dc1466307328e
Karen Ramirez <karen.bussines@hotmail.com>
2020-05-06 17:31:37 -0500

    [HOME] se elabora el componente del home en donde se muestran los personajes a filtrar

--- page-movies/src/Home.js
+++ page-movies/src/Home.js
@@ -1,9 +1,22 @@
 import React from "react";
+import NavBar from './Components-JS/Navbar';
+import Styles from './Components-CSS/Home.module.css'
 
 function Home (){
     return(
         <div>
-            hola
+            <NavBar/>
+            <h1 className={Styles.title}>
+                How is the Best?
+            </h1>
+            <div className={Styles.avatars} >
+                <div>
+                    <img className={Styles.avatarSuperMan} alt='icons' src='https://image.flaticon.com/icons/png/512/1674/1674292.png'/>
+                </div>
+                <div>
+                    <img  className={Styles.avatarBatman} alt='icons' src='https://image.flaticon.com/icons/png/512/1674/1674291.png'/>
+                </div>
+            </div>
         </div>
 
     )
