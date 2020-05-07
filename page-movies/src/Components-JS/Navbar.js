11253a2489159b710b5e388603cf62aae914c6a8
GeorginaPQ <giorginapqe@gmail.com>
2020-05-07 01:07:22 -0500

    [Navbar] agregando estilos

--- page-movies/src/Components-JS/Navbar.js
+++ page-movies/src/Components-JS/Navbar.js
@@ -1,61 +1,14 @@
-import React, {useState} from 'react';
-
+import React from 'react';
 import Image  from '../Components-CSS/img/logo.svg';
-import '../Components-CSS/Navbar.css'
+import '../Components-CSS/Navbar.css';
 
 
 const Navbar = ({dataItem}) => {
 
-const showScore = () => {
-
-}
     return (
-    <div className="container">
-
-        <nav className="navbar navbar-toggler navbar-light bg-warning bg-warning px-0 fixed-top">
+    <div className="container-fluid p-0 nav-cont">
+        <nav className="navbar navbar-toggler black p-0 m-0 border-0 d-flex justify-content-center">
             <img className="navbar-brand logoSmall" src={Image} />
-
-
-            <button 
-            className="navbar-toggler" 
-            type="button" 
-            data-toggle="collapse" 
-            data-target="#navbarSupportedContent" 
-            aria-controls="navbarSupportedContent" 
-            aria-expanded="false" 
-            aria-label="Toggle navigation" 
-            onClick={console.log('hola')} 
-            >
-            <span className="navbar-toggler-icon"></span>
-            </button>
-        
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            
-                <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#" onClick={showScore}>Score Movies</a>
-                    </li> 
-                    <li className="nav-item active">
-                    <a className="nav-link" href="#">Recent Movies</a>
-                    </li>
-                </ul>
-
-               {/*  <form className="serch-form"
-                    onSubmit={ (e) => { 
-                        e.preventDefault()
-                        getMovie()
-                    }}    
-                >        
-                    <input className="serch-bar" type="text" 
-                    onChange={((e)=>{         
-                    setSearchName(e.target.value)
-                    })}/>
-                    <button className="serch-button" type="submit">
-                    Search
-                    </button>
-             </form> */}
-
-            </div>
         </nav> 
     </div>
 
--- page-movies/src/Home.js
+++ page-movies/src/Home.js
@@ -8,22 +8,17 @@ import { Link } from 'react-router-dom'
 function Home (){
     return(
         <div className={Styles.home}>
-            <NavBar/>
             <h1 className={Styles.title}>
                 Who is the Best?
             </h1>
             <div className={Styles.avatars} >
-                <div>
-                    <Link to='/Superman'>
-                    <img  className={Styles.avatarSuperMan} alt='icons' src='https://image.flaticon.com/icons/png/512/1674/1674292.png'/>
+                    <Link className={Styles.links  } to='/Superman'>
+                    <img  className={Styles.avatarSuperMan  } alt='icons' src='https://image.flaticon.com/icons/png/512/1674/1674292.png'/>
                     </Link>
-                </div>
                 
-                <div>
                     <Link to='/Batman'>
                         <img  className={Styles.avatarBatman} alt='icons' src='https://image.flaticon.com/icons/png/512/1674/1674291.png'/>
                     </Link>
-                </div>
             </div>
         </div>
 
